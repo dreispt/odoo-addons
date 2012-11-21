84f6ec0d91ee3bd6f9cf524129010480be32fe29
Daniel Reis <dreis.pt@hotmail.com>
2012-11-21 16:45:56 +0000

    ADD: view codes for analysic accounts/contracts

---
+++ analytic_refcodes/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    
+#    Copyright (C) 2012 Daniel Reis
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Project code visible and searchable in form fields',
+    'version': '1.0',
+    'category': 'Project Management',
+    'author': 'Daniel Reis',
+    'website': 'daniel.reis@securitas.pt',
+    'depends': ['base_util_refcodes', 'analytic', ],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ analytic_refcodes/analytic.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    
+#    Copyright (C) 2012 Daniel Reis
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+from base_util_refcodes import name_tools
+
+class account_analytic_account(osv.osv):
+    _inherit = 'account.analytic.account'
+    
+    def name_get(self, cr, uid, ids, context=None):
+        return name_tools.extended_name_get(self, cr, uid, ids, '[%(code)s] %(name)s', ['code', 'name'], context=context)
+        
+    def name_search(self, cr, user, name='', args=None, operator='ilike', context=None, limit=100):
+        return name_tools.extended_name_search(self, cr, user, name, args, operator, context=context, limit=limit, 
+                                keys=['code', 'name'])
+    
+account_analytic_account()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+
