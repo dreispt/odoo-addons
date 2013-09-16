121280cf4fe1d26449026661f490357c8a3ce51b
Daniel Reis <dgreis@sapo.pt>
2013-09-16 17:17:24 +0100

    Remove forgotten print statement

--- base_util_refcodes/name_tools.py
+++ base_util_refcodes/name_tools.py
@@ -46,7 +46,6 @@ def extended_name_get(obj, cr, uid, ids, name_mask, flds_templ, context=None):
 
     res = []
     for rec in obj.read(cr, uid, ids, flds_templ, context=context):
-        print flds_templ, rec
         for key in flds_templ:
             if not rec[key]:
                 # Empty values iare replaced by empty string
